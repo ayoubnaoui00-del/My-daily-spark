d7d99f730e26a59f93daf4c78ab56db482bd7356
ayoubnaoui00-del <ayoubnaoui00@gmail.com>
2026-05-06 10:21:20 +0100

    logo

--- spark/src/components/header.tsx
+++ spark/src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () =>
     return <>
     <div className="flex justify-between  w-11/12 mx-auto  ">
 
-        <img className= "w-1/9 flex "  src="/sparkImg.png"/>
+        <img className= "w-1/9 flex "  src="/logo.png"/>
 
         <ul className="flex gap-4  justify-end items-center">
             <li>today</li>
